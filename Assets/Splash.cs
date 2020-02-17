d1562ec7384f530abda442e8824dbecb8eed0414
Michael Numrich <mnumrich@gmail.com>
2020-02-16 17:15:04 -0700

    Created splash screen script

--- Assets/Splash.cs
+++ Assets/Splash.cs
@@ -5,12 +5,15 @@ using UnityEngine.SceneManagement;
 
 public class Splash : MonoBehaviour
 {
+    private void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
     // Start is called before the first frame update
     void Start()
     {
         Invoke("StartGame", 4);
     }
-
     public void StartGame()
     {
         SceneManager.LoadScene(1);
